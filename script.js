6729db2574b1a3220742cc131ed72beeacf29129
ZyaxitronED <edwardthornton624@gmail.com>
2022-10-29 19:11:39 +0100

    Displaying .txt files in textarea
    Began with simple reading of .txt files and displaying it in a textarea.
    .docx files are returning all characters as unknown.
    Will run checks on the printed information such as word count, line count, most common letter, and mean, mode and median letters per word.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+function textToArea() {
+    let input = document.querySelector('input');
+    let textarea = document.querySelector('textarea');
+
+    let files = input.files;
+
+    if (files.length == 0) return;
+
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        textarea.value = lines.join('\n');
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+};
