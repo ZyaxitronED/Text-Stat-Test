5e39eff2b1407614e750b8215cc9f025548fbdf4
ZyaxitronED <edwardthornton624@gmail.com>
2022-10-30 01:41:34 +0000

    Counts words and characters in textarea.
    On button press, app will evaluate text in the text area, display the word count excluding spaces and the character count excluding spaces.

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 function textToArea() {
     let input = document.querySelector('input');
     let textarea = document.querySelector('textarea');
-
     let files = input.files;
 
     if (files.length == 0) return;
 
     const file = files[0];
-
     let reader = new FileReader();
 
     reader.onload = (e) => {
@@ -18,6 +16,15 @@ function textToArea() {
     }
 
     reader.onerror = (e) => alert(e.target.error.name);
-
     reader.readAsText(file);
+};
+
+function countWords() {
+    var textareaValue = document.querySelector('textarea').value;
+    var regex = /\s+/gi;
+    var wordCount = textareaValue.trim().replace(regex, ' ').split(' ').length;
+    var charCount = textareaValue.replace(regex, '').length;
+
+    document.getElementById('wordCount').innerHTML = "Word Count (Whitespace Delimited): " + wordCount;
+    document.getElementById('charCount').innerHTML = "Character Count (Excluding Spaces): " + charCount;
 };
